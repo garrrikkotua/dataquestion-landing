ddfb0bd75e4f9243d826234582dbf427f4428327
garrrikkotua <kotuaigor@gmail.com>
2023-07-01 17:46:44 +0300

    add vercel ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
-
 import alpinejs from "@astrojs/alpinejs";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact(), alpinejs()]
+  integrations: [tailwind(), preact(), alpinejs()],
+  output: "hybrid",
+  adapter: vercel()
 });
